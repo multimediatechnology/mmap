9a82777111bac0d0d23b3d624275a4510f7f2cee
Hannes Moser <hannes@impossiblearts.com>
2017-04-01 20:24:01 +0200

    chore: ingore mailer errors until mail gateway is ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # as devise setup instruction says
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
