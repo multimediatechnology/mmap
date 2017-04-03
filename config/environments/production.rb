6986d1a2928f42a11d74318a8a41a7873fa641a9
Hannes Moser <hannes@impossiblearts.com>
2017-04-03 14:35:23 +0200

    fix: missing default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # as devise setup instruction says
+  config.action_mailer.default_url_options = { host: 'multimediaart.at', port: 443 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'mail-proj.fh-salzburg.ac.at',
