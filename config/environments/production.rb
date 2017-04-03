e94f849e08dc8c631c158ce816d7057f5c4280c4
Hannes Moser <hannes@impossiblearts.com>
2017-04-03 13:48:05 +0200

    feat: use real mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,12 +60,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mmap_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
-
   # as devise setup instruction says
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'mail-proj.fh-salzburg.ac.at',
+    port: 25
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
