1e5dff66f063cc2f42c763f70f0728b16e268b00
Hannes Moser <hannes@impossiblearts.com>
2018-04-13 17:28:47 +0200

    fix: dropzone upload timeout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,47 +23,49 @@ Dropzone.autoDiscover = false
 
 $(document).on('turbolinks:load', function () {
 
-    // grap our upload form by its id
-    $('.dropzone').dropzone({
-        // restrict image size to a maximum 1GB
-        maxFilesize: 1 * 1024,
-        // max files (1)
-        maxFiles: 1,
-        // change default message
-        dictDefaultMessage: 'Datei reinziehen oder klicken.',
-        // changed the passed param to one accepted by
-        // our rails app
-        paramName: 'asset[file]',
-        // show remove links on each image upload
-        addRemoveLinks: false,
-        // if the upload was successful
-        success: function (file, response) {
-            // find the remove button link of the uploaded file and give it an id
-            // based of the fileID response from the server
-            $(file.previewTemplate)
-                .find('.dz-remove')
-                .attr('id', response.fileID)
-            // add the dz-success class (the green tick sign)
-            $(file.previewElement)
-                .addClass('dz-success')
-            // replace table with list of files
-            $('#files').html(response)
-        },
-        //when the remove button is clicked
-        removedfile: function (file) {
-            // grap the id of the uploaded file we set earlier
-            const id = $(file.previewTemplate)
-                .find('.dz-remove')
-                .attr('id')
+  // grap our upload form by its id
+  $('.dropzone').dropzone({
+    // restrict image size to a maximum 1GB
+    maxFilesize: 2 * 1024,
+    // max files (1)
+    maxFiles: 1,
+    // timeout
+    timeout: 86400,
+    // change default message
+    dictDefaultMessage: 'Datei reinziehen oder klicken.',
+    // changed the passed param to one accepted by
+    // our rails app
+    paramName: 'asset[file]',
+    // show remove links on each image upload
+    addRemoveLinks: false,
+    // if the upload was successful
+    success: function (file, response) {
+      // find the remove button link of the uploaded file and give it an id
+      // based of the fileID response from the server
+      $(file.previewTemplate)
+        .find('.dz-remove')
+        .attr('id', response.fileID)
+      // add the dz-success class (the green tick sign)
+      $(file.previewElement)
+        .addClass('dz-success')
+      // replace table with list of files
+      $('#files').html(response)
+    },
+    //when the remove button is clicked
+    removedfile: function (file) {
+      // grap the id of the uploaded file we set earlier
+      const id = $(file.previewTemplate)
+        .find('.dz-remove')
+        .attr('id')
 
-            // make a DELETE ajax request to delete the file
-            $.ajax({
-                type: 'DELETE',
-                url: '/assets/' + id,
-                success: function (data) {
-                    console.log(data.message)
-                }
-            })
+      // make a DELETE ajax request to delete the file
+      $.ajax({
+        type: 'DELETE',
+        url: '/assets/' + id,
+        success: function (data) {
+          console.log(data.message)
         }
-    })
+      })
+    }
+  })
 })
